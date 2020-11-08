d54af9f6c683c2ea1751515ce231b7abaf2967c5
fetisov <fetisov@DESKTOP-0FL4TAM>
2020-11-08 21:10:02 +0300

    change default color

--- Icon.cs
+++ Icon.cs
@@ -8,7 +8,7 @@ namespace Feather.Blazor
     public class Icon : ComponentBase
     {
         [Parameter]
-        public string Color { get; set; } = "#ffffff";
+        public string Color { get; set; } = "currentColor";
         [Parameter]
         public float StrokeWidth { get; set; } = 1.5f;
         [Parameter]
